29e33fb641480970bcea6b3c9715ce571c77744e
Jaeeun Park <pjcson8@naver.com>
2025-02-16 11:29:51 +0900

    CHORE: JPA 설정 JpaConfig로 이동

--- src/main/java/com/spring/delivery/global/config/JpaConfig.java
+++ src/main/java/com/spring/delivery/global/config/JpaConfig.java
@@ -3,7 +3,9 @@ package com.spring.delivery.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 @Configuration
 public class JpaConfig {
 
