a27c26ccd8ab06786147927d69f574417db13e87
Jaeeun Park <pjcson8@naver.com>
2025-02-24 15:43:34 +0900

    REFACTOR: IntegrationTestBase 상속받아 teardown 실행

--- src/test/java/com/spring/delivery/domain/service/MenuServiceTest.java
+++ src/test/java/com/spring/delivery/domain/service/MenuServiceTest.java
@@ -28,7 +28,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@ActiveProfiles("test")
 class MenuServiceTest {
 
     @Autowired
